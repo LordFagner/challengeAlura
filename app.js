2199cdfc972ab23c5e8f9b6d02ce613ab5e660ee
Dev_fag_corp <fagner.compras1.0.0@gmail.com>
2025-01-28 01:39:32 -0300

    finalizando desafio

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let amigos = [];
-
+let lista = document.getElementById("listaAmigos");
+let resultados = document.getElementById("resultado");
 
 
 function adicionarAmigo() {
     console.log
     let conteudo = document.getElementById("amigo");
-    console.log(conteudo.tagName);
+    // console.log(conteudo.tagName);
 
     if (conteudo != null ) {
         let amigo = conteudo.value.trim();
@@ -14,13 +15,26 @@ alert("insira um valor valido")
         }else{
         amigos.push(amigo);
 
-        let lista = document.getElementById("listaAmigos");
         let item = document.createElement("li");
         item.textContent = amigo;
         lista.appendChild(item);
         conteudo.value = "";
+        resultados.innerHTML = "";
     }
 
 }
+
+}
+
+function sortearAmigo(){
+
+let Sorteio  = Math.floor(Math.random() * amigos.length); 
+// console.log(Sorteio);
+
+let SorteadoMensagem ="o amigo secreto Sorteado foi : " + amigos[Sorteio];
+let item = document.createElement("li");
+item.textContent = SorteadoMensagem;
+resultados.appendChild(item);
+lista.innerHTML = "";
 
 }
