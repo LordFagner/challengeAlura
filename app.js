01fada40675ebd60633e235c2fcb1925123e35db
Dev_fag_corp <fagner.compras1.0.0@gmail.com>
2025-01-28 01:20:39 -0300

    adicionando verificação de texto

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
- let amigos = [];
+let amigos = [];
 
 
 
-function adicionarAmigo(){
+function adicionarAmigo() {
+    console.log
+    let conteudo = document.getElementById("amigo");
+    console.log(conteudo.tagName);
 
-let conteudo = document.querySelector('input').value;
-if(conteudo != null){
-amigos.push(conteudo);
+    if (conteudo != null ) {
+        let amigo = conteudo.value.trim();
+        if(amigo == ""){
+alert("insira um valor valido")
+        }else{
+        amigos.push(amigo);
 
-let lista = document.getElementById("listaAmigos"); 
-let item = document.createElement("li"); 
-item.textContent(conteudo);
-lista.appendChild(item);
-
-
-conteudo.value = " ";
-conteudo.focus(); 
-}else{
-    alert("Por Favor Insira Um Nome Valido !!!!!"); 
+        let lista = document.getElementById("listaAmigos");
+        let item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+        conteudo.value = "";
+    }
 
 }
-
-
-
-
 
 }
